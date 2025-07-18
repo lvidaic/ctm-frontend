f9d1046ee80d13f6b2f483fe11920f1322e77f10
Leo Vidaic <leoo.vidaic@gmail.com>
2025-07-18 22:03:34 +0200

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-// import './App.css'
 import Provider from './pages/Provider.jsx'
-
+import { Routes, Route } from 'react-router';
 
 function App() {
 
     return (
-        <>
+        <div>
             <h2>Call The Medic</h2>
-            <Provider />
-        </>
+            <Routes>
+                <Route path="provider" element={<Provider />} />
+            </Routes>
+        </div>
     )
 }
 

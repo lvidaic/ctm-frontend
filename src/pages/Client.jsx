fbe5e3e76c0a1967ccf9b6ef1a27b5b98c65257d
Leo Vidaic <leoo.vidaic@gmail.com>
2025-07-21 16:14:38 +0200

    added data loading

--- src/pages/Client.jsx
+++ src/pages/Client.jsx
@@ -1,6 +1,6 @@
 import useSWR from "swr"
 import ClientEditor from '../components/ClientEditor.jsx';
-import { fetcher, imageFetcher } from '../utils/fetchers.js';
+import { fetcher } from '../utils/fetchers.js';
 
 export default function Client() {
 
@@ -8,15 +8,18 @@ export default function Client() {
         console.log(createdClient);
     }
 
-    // const { data: client } = useSWR("http://localhost:8080/api/clients/ff3f05dc-305e-4040-a6b1-5697bcc8c08f", fetcher); const { data: image } = useSWR(() => "http://localhost:8080/api/images/image", imageFetcher, {objectName: ff3f05dc-305e-4040-a6b1-5697bcc8c08f/SqaureCoding.JPG"});
-    const { data: image } = useSWR({ url: "http://localhost:8080/api/images/image", data: "ff3f05dc-305e-4040-a6b1-5697bcc8c08f/SqaureCoding.JPG" }, (args) => imageFetcher(args));
-    console.log(image);
+    const { data: client, error, isLoading } = useSWR("http://localhost:8080/api/clients/ff3f05dc-305e-4040-a6b1-5697bcc8c08f", fetcher);
 
-    if (!image) return 'loading'
+    if (error) {
+        return <div>Error while loading data</div>
+    }
+
+    if (isLoading) {
+        return <div>Loading data</div>
+    }
 
     return (
-        loaded
-        // <ClientEditor client={client} onSave={save} />
+        <ClientEditor client={client} onSave={save} />
     )
 }
 
